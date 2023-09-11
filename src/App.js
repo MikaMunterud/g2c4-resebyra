3b71533193c0b8b1170708cbde8293e0871b34fb
tsorak <karost0249@gmail.com>
2023-09-11 13:58:37 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
 import Homepage from "./pages/Homepage";
+import Singlepost from "./pages/Singlepost.js";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/post/:index" element={<Singlepost />} />
+    </>
+  )
+);
 
 function App() {
-  return (
-    <div className="App">
-      <Homepage />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
